2fc5979402787f2016c70dc73ba486a87b8f40a1
FevenSeyfu <fevenseyfu00@gmail.com>
2024-04-24 00:32:48 +0300

    add active state style for home page

--- React/src/pages/Home.jsx
+++ React/src/pages/Home.jsx
@@ -3,9 +3,9 @@ import '../assets/Style/Home.css'
 
 const Home = () => {
   return (
-    <div className=" bg-[url('/src/assets/home/background-home-desktop.jpg')] bg-no-repeat bg-cover bg-center w-screen h-screen text-white">
+    <div className="bg-[url('/src/assets/home/background-home-desktop.jpg')] bg-no-repeat bg-cover bg-center w-screen h-screen text-white ">
       <main className=" flex flex-row">
-        <article className="flex flex-col absolute top-[387px] left-[165px] w-[480px] h-[382px] gap-[24px]">
+        <article className="flex flex-col absolute bottom-[14.56%] left-[165px] w-[480px] h-[382px] gap-[24px]">
           <p className="font-barlow uppercase text-[28px] tracking-[4.72px] text-[#D0D6F9]">So, you want to travel to</p>
           <h1 className="home-heading">SPACE</h1>
           <p className="home-text">
@@ -15,7 +15,7 @@ const Home = () => {
           experience!
           </p>
         </article>
-        <button class="CTA-button">Explore</button>
+        <button className="CTA-button">Explore</button>
       </main>
     </div>
   );
