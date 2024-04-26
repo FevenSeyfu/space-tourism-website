37971ada411f4a458689668f52a626bd7ffa4df2
FevenSeyfu <fevenseyfu00@gmail.com>
2024-04-26 16:32:53 +0300

    fix destination page image

--- React/src/pages/Destination.jsx
+++ React/src/pages/Destination.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import moonImage from "../assets/destination/image-moon.png";
 import data from "../../data.json";
 import Layout from "../components/Layout";
 import backgroundImageLg from "../assets/destination/background-destination-desktop.jpg";
@@ -8,7 +7,7 @@ import backgroundImageSm from "../assets/destination/background-destination-mobi
 
 const Destination = () => {
   const [destinations, setDestinations] = useState([]);
-  const [selectedDestination, setSelectedDestination] = useState(null);
+  const [selectedDestinationIndex, setSelectedDestinationIndex] = useState(0);
   const [backgroundImage, setBackgroundImage] = useState(backgroundImageSm);
 
   useEffect(() => {
@@ -35,8 +34,8 @@ const Destination = () => {
     setSelectedDestination(data.destinations[0]);
   }, []);
 
-  const handleDestinationClick = (destination) => {
-    setSelectedDestination(destination);
+  const handleDestinationClick = (index) => {
+    setSelectedDestinationIndex(index);
   };
 
   return (
@@ -50,20 +49,20 @@ const Destination = () => {
           Pick your destination
         </h1>
         <article className="flex flex-col w-full items-center  gap-6 lg:gap-4 lg:flex-row lg:items-end lg:justify-evenly lg:h-[472px] lg:w-auto lg:mt-[4rem] lg:pb-4">
-          {selectedDestination && (
+          {destinations[selectedDestinationIndex] && (
             <>
               <img
-                src={selectedDestination.images.png}
-                alt={`${selectedDestination.name} Image`}
+                src={destinations[selectedDestinationIndex].images.png}
+                alt={`${destinations[selectedDestinationIndex].name} Image`}
                 className="w-[170px] h-[170px] md:w-[300px] md:h-[300px] lg:w-[27.8rem] lg:h-[27.8rem] lg:ml-[63.5px]"
               />
               <div className="flex flex-col md:mx-[60px] lg:w-[444px] lg:ml-[157px] lg:gap-4">
                 <ul className="flex flex-row px-10 md:px-32  lg:p-0 justify-evenly lg:w-[285.5px] lg:h-[24px] lg:gap-[35px] font-normal text-[#D0D6F9] text-sm tracking-[2.36px] md:text-base md:tracking-[2.7px]  uppercase ">
-                  {destinations.map((destination) => (
+                  {destinations.map((destination,index) => (
                     <li
-                      key={destination.name}
+                      key={index}
                       className={`hover:underline underline-offset-[12px] decoration-[3px] hover:text-[#D0D6F9] hover:cursor-pointer hover:opacity-[0.5] mb-5 md:mb-8 font-barlowCondensed  ${
-                        destination.name === selectedDestination.name
+                        index === selectedDestinationIndex
                           ? "underline text-white"
                           : ""
                       }`}
@@ -72,32 +71,33 @@ const Destination = () => {
                         href="#"
                         onClick={(e) => {
                           e.preventDefault();
-                          handleDestinationClick(destination);
+                          handleDestinationClick(index);
+                          
                         }}
                       >
-                        {destination.name}
+                        {destinations[selectedDestinationIndex].name}
                       </a>
                     </li>
                   ))}
                 </ul>
                 <h2 className="font-bellefair  uppercase font-normal text-white text-[56px] md:text-[80px] lg:text-[100px]">
-                  {selectedDestination.name}
+                  {destinations[selectedDestinationIndex].name}
                 </h2>
                 <p className=" font-barlow font-normal text-[#D0D6F9] text-[15px] md:text-base lg:text-lg leading-6 md:leading-7  lg:leading-8 lg:tracking-widest  lg:mx-0 ">
-                  {selectedDestination.description}
+                  {destinations[selectedDestinationIndex].description}
                 </p>
                 <div className="w-full h-[1px] bg-[#383B4B] my-8 md:mt-12 md:mb-7  lg:mt-[54px] lg:mb-0" />
                 <div className="flex flex-col gap-8 md:flex-row md:justify-evenly lg:justify-start lg:gap-[79px] font-normal font-barlowCondensed tracking-wider  uppercase">
                   <p className="flex flex-col gap-3  text-[#D0D6F9] text-sm ">
                     Avg. distance
                     <span className="font-bellefair  text-[28px] text-white">
-                      {selectedDestination.distance}
+                      {destinations[selectedDestinationIndex].distance}
                     </span>
                   </p>
                   <p className="flex flex-col gap-3 text-[#D0D6F9] text-sm">
                     Est. travel time
                     <span className="font-bellefair text-[28px] text-white">
-                      {selectedDestination.travel}
+                      {destinations[selectedDestinationIndex].travel}
                     </span>
                   </p>
                 </div>
